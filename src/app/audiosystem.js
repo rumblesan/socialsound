8fda2b0c2e7c0a2d8d99f6f2cdc14149c56f3459
Guy John <guy@rumblesan.com>
2015-04-21 13:29:26 +0100

    Adding audio system and hooking it up

--- src/app/audiosystem.js
+++ src/app/audiosystem.js
@@ -13,7 +13,65 @@ var createContext = function (w) {
     return context;
 };
 
-var createAudioSystem = function() {
+var createAudioSystem = function(audioContext) {
+
+    var AudioSystem = {};
+
+    AudioSystem.createVoice = function () {
+
+        var env = {};
+        env.attack = 0.1;
+        env.release = 0.2;
+
+        var osc = audioContext.createOscillator();
+        osc.type = 'square';
+        osc.frequency.value = 440;
+        osc.start();
+
+        var filter = audioContext.createBiquadFilter();
+        filter.type = 'lowpass';
+        filter.frequency.value = 200;
+        filter.Q.value = 1.2;
+
+        var amp = audioContext.createGain();
+        amp.gain.value = 0;
+
+        osc.connect(filter);
+        filter.connect(amp);
+
+        var voice = {};
+
+        voice.outputNode = amp;
+
+        voice.play = function (freq, length) {
+            console.log('playing', freq, length);
+            osc.frequency.value = freq;
+            var gainParam = amp.gain;
+            gainParam.linearRampToValueAtTime(
+                1,
+                audioContext.currentTime + env.attack
+            );
+            gainParam.linearRampToValueAtTime(
+                1,
+                audioContext.currentTime + length
+            );
+            gainParam.linearRampToValueAtTime(
+                0,
+                audioContext.currentTime + length + env.release
+            );
+        };
+
+        return voice;
+    };
+
+    AudioSystem.connectVoices = function(voices) {
+        var i;
+        for (i = 0; i < voices.length; i += 1) {
+            voices[i].outputNode.connect(audioContext.destination);
+        }
+    };
+
+    return AudioSystem;
 };
 
 module.exports = {
--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@
 var $ = require('./lib/jquery-2.1.3');
 var Grid = require('./app/grid');
 var Seq = require('./app/sequencer');
+var Audio = require('./app/audiosystem');
 
 $(function () {
 
     var canvas = document.getElementById('display');
+
+    var ctx = Audio.createContext(window);
+
+    var audio = Audio.createAudio(ctx);
 
     var sequencer = Seq.create();
     var grid = Grid.create();
@@ -21,6 +26,22 @@ $(function () {
     });
 
     sequencer.setBpm(120);
+
+    var voiceNum = 12;
+    var voices = [];
+    var v;
+    for (v = 0; v < voiceNum; v += 1) {
+        voices.push(audio.createVoice())
+    }
+    audio.connectVoices(voices);
+
+    var playFunc = function (note) {
+        var n = note + 60; // increase octave, 60 is middle C
+        var f = Math.pow(2, ((n - 69)/12)) * 440;
+        console.log('playing note', note, n, f);
+        voices[note].play(f, 1);
+    };
+    grid.setPlayFunc(playFunc);
 
     var processingInstance = new Processing(canvas, grid.sketch);
 
